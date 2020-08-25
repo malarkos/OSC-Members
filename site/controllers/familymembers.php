396ad40abb34638ce36b86fe239fd9a4f4333a00
Geoff Markley <geoffm@labyrinth.net.au>
2020-08-25 13:00:44 +1000

    Adding in family members - copy of members - to be updated.

---
+++ site/controllers/familymembers.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+require_once JPATH_COMPONENT . '/controller.php';
+
+/**
+ * Profile controller class for Users.
+ *
+ * @since  1.6
+ */
+class MembersControllerMembers extends MembersController 
+{
+	/**
+	 * Method to check out a user for editing and redirect to the edit form.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.6
+	 * 
+	 */
+	  public function edit()
+	{
+		$app         = JFactory::getApplication();
+		$user        = JFactory::getUser();
+		$loginUserId = (int) $user->get('id');
+		
+		// Get the model.
+		$model = $this->getModel('Members', 'MembersModel');
+		
+		// Redirect to the edit screen.
+		$this->setRedirect(JRoute::_('index.php?option=com_members&view=members&layout=edit', false));
+		
+		return true;
+	 } // edit
+	 
+	 /**
+	  * Method to save a user's profile data.
+	  *
+	  * @return  void
+	  *
+	  * @since   1.6
+	  */
+	 public function save()
+	 {
+	     
+	    // JFactory::getApplication()->enqueueMessage('In save:');
+	     
+	 	// Check for request forgeries.
+	 	JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+	 
+	 	$app    = JFactory::getApplication();
+	 	$model = $this->getModel('Members', 'MembersModel');
+	 	
+	 	// Get the user data.
+	 	$data = $app->input->post->get('jform', array(), 'array');
+	 	//JFactory::getApplication()->enqueueMessage('form data'.$data);
+	 	
+	 	// Validate the posted data.
+	 	$form = $model->getForm();
+	 	
+	 	if (!$form)
+	 	{
+	 		JError::raiseError(500, $model->getError());
+	 	
+	 		return false;
+	 	}
+	 	
+	 	// Validate the posted data.
+	 	$data = $model->validate($form, $data);
+	 	
+	 	/*// Check for errors.
+	 	if ($data === false)
+	 	{
+	 		// Get the validation messages.
+	 		$errors = $model->getErrors();
+	 	
+	 		// Push up to three validation messages out to the user.
+	 		for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++)
+	 		{
+	 			if ($errors[$i] instanceof Exception)
+	 			{
+	 				$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+	 			}
+	 			else
+	 			{
+	 				$app->enqueueMessage($errors[$i], 'warning');
+	 			}
+	 		}
+	 	
+	 		// Save the data in the session.
+	 		$app->setUserState('com_members.edit.members.data', $data);
+	 	
+	 		// Redirect back to the edit screen.
+	 		$userId = (int) $app->getUserState('com_members.edit.members.id');
+	 		$this->setRedirect(JRoute::_('index.php?option=com_members&view=members&layout=edit', false));
+	 	
+	 		return false;
+	 	}*/
+	 	
+	 	// Attempt to save the data.
+	 	$return = $model->save($data);
+	 	
+	 	
+	 	// Check for errors.
+	 	if ($return === false)
+	 	{
+	 		// Save the data in the session.
+	 		$app->setUserState('com_members.edit.members.data', $data);
+	 	
+	 		// Redirect back to the edit screen.
+	 		$this->setMessage(JText::sprintf('COM_MEMBERS_MEMBERS_SAVE_FAILED', $model->getError()), 'warning');
+	 		$this->setRedirect(JRoute::_('index.php?option=com_members&view=members&layout=edit', false));
+	 	
+	 		return false;
+	 	}
+	 	
+	 	// Flush the data from the session.
+	 	$app->setUserState('com_members.edit.members.data', null);
+	 	
+	 	// Set return path
+	 	$this->setRedirect(JRoute::_('index.php?option=com_members&view=members', false));
+	 	
+	 } // save
+} // class
---
+++ site/views/familymembers/tmpl/default.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_members
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
+
+?>
+<h3>Member details</h3>
+<A href="<?php echo JRoute::_('index.php?option=com_members&view=members&layout=edit'); ?>">Update details</A>
+<table class="table table-striped table-hover">
+	<tbody>
+		<tr>
+			<td>Member:</td>
+			<td><?php echo $this->data->MemberFirstname; ?> <?php echo $this->data->MemberSurname; ?></td>
+		</tr>
+		<tr>
+			<td>Member Type:</td>
+			<td><?php echo $this->data->MemberType; ?> </td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td><?php echo $this->data->useremail; ?> (Edit in 
+			<A href="<?php $url = Uri::root() . 'index.php/your-profile'; echo $url; ?>">Your Profile</A>)</td>
+		</tr>
+		<tr>
+			<td>Address:</td>
+			<td><?php echo $this->data->MemberHomeAddress; ?>, <?php echo $this->data->MemberHomeAddress2; ?><br />
+			<?php echo $this->data->MemberHomeSuburb; ?><br />
+			<?php echo $this->data->MemberHomeState; ?> <?php echo $this->data->MemberHomePostcode; ?><br />
+			<?php echo $this->data->MemberHomeCountry; ?>
+			 </td>
+		</tr>
+		<tr>
+			<td >Mobile:</td>
+			<td ><?php echo $this->data->MemberPhoneMobile; ?> </td>
+		</tr>
+		<tr>
+			<td >Home:</td>
+			<td ><?php echo $this->data->MemberPhoneHome; ?> </td>
+		</tr>
+		<tr>
+			<td >Work:</td>
+			<td ><?php echo $this->data->MemberPhoneWork; ?> </td>
+		</tr>
+		<tr>
+			<td >Date of Birth:</td>
+			<td ><?php echo $this->data->memberdob; ?> </td>
+		</tr>
+	</tbody>
+</table>
+ 		
